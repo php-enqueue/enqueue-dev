1dbc8bd6cbdb73da066381b7a6d37c171f057f79
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-05-13 23:02:15 +0300

    add dsn_to_connection_factory and dsn_to_context functions.

---
+++ pkg/enqueue/Tests/Functions/DsnToConnectionFactoryFunctionTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Enqueue\Tests\Router;
+
+use Enqueue\AmqpExt\AmqpConnectionFactory;
+use Enqueue\Fs\FsConnectionFactory;
+use Enqueue\Null\NullConnectionFactory;
+use PHPUnit\Framework\TestCase;
+
+class DsnToConnectionFactoryFunctionTest extends TestCase
+{
+    public function testThrowIfDsnEmpty()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('The scheme could not be parsed from DSN ""');
+
+        \Enqueue\dsn_to_connection_factory('');
+    }
+
+    public function testThrowIfDsnMissingScheme()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('The scheme could not be parsed from DSN "dsnMissingScheme"');
+
+        \Enqueue\dsn_to_connection_factory('dsnMissingScheme');
+    }
+
+    public function testThrowIfDsnNotSupported()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('The scheme "http" is not supported. Supported "file", "amqp", "null"');
+
+        \Enqueue\dsn_to_connection_factory('http://schemeNotSupported');
+    }
+
+    /**
+     * @dataProvider provideDSNs
+     *
+     * @param mixed $dsn
+     * @param mixed $expectedFactoryClass
+     */
+    public function testReturnsExpectedFactoryInstance($dsn, $expectedFactoryClass)
+    {
+        $factory = \Enqueue\dsn_to_connection_factory($dsn);
+
+        $this->assertInstanceOf($expectedFactoryClass, $factory);
+    }
+
+    public static function provideDSNs()
+    {
+        yield ['amqp://', AmqpConnectionFactory::class];
+
+        yield ['amqp://user:pass@foo/vhost', AmqpConnectionFactory::class];
+
+        yield ['file://', FsConnectionFactory::class];
+
+        yield ['file://foo/bar/baz', FsConnectionFactory::class];
+
+        yield ['null://', NullConnectionFactory::class];
+    }
+}
