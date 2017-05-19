abe8579574a06e001e1ef41503ecceab79c03479
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-05-19 10:56:30 +0300

    Async events. Add docs.

--- pkg/enqueue-bundle/Events/EventTransformer.php
+++ pkg/enqueue-bundle/Events/EventTransformer.php
@@ -3,6 +3,7 @@
 namespace Enqueue\Bundle\Events;
 
 use Enqueue\Client\Message;
+use Enqueue\Consumption\Result;
 use Enqueue\Psr\PsrMessage;
 use Symfony\Component\EventDispatcher\Event;
 
@@ -17,10 +18,13 @@ interface EventTransformer
     public function toMessage($eventName, Event $event = null);
 
     /**
+     * If you able to transform message back to event return it.
+     * If you failed to transform for some reason you can return instance of Result object ( Like this Result::reject() );.
+     *
      * @param string     $eventName
      * @param PsrMessage $message
      *
-     * @return Event|null
+     * @return Event|Result|null
      */
     public function toEvent($eventName, PsrMessage $message);
 }
