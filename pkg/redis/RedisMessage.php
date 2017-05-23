76fc474690dfe1fd19575ad97c1a11f40310e293
Konstantin.Myakshin <koc-dp@yandex.ru>
2017-05-23 14:04:22 +0300

    Reuse json util where it possible

--- pkg/redis/RedisMessage.php
+++ pkg/redis/RedisMessage.php
@@ -3,6 +3,7 @@
 namespace Enqueue\Redis;
 
 use Enqueue\Psr\PsrMessage;
+use Enqueue\Util\JSON;
 
 class RedisMessage implements PsrMessage, \JsonSerializable
 {
@@ -219,14 +220,7 @@ class RedisMessage implements PsrMessage, \JsonSerializable
      */
     public static function jsonUnserialize($json)
     {
-        $data = json_decode($json, true);
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw new \InvalidArgumentException(sprintf(
-                'The malformed json given. Error %s and message %s',
-                json_last_error(),
-                json_last_error_msg()
-            ));
-        }
+        $data = $data = JSON::decode($json);
 
         return new self($data['body'], $data['properties'], $data['headers']);
     }
