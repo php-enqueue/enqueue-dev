0fda67601150782b5c999982c65f08cca74aa951
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-09 13:35:53 +0300

    incr retries for kafka test.

--- pkg/rdkafka/Tests/Spec/RdKafkaSendToAndReceiveFromTopicTest.php
+++ pkg/rdkafka/Tests/Spec/RdKafkaSendToAndReceiveFromTopicTest.php
@@ -8,7 +8,7 @@ use Interop\Queue\Spec\SendToAndReceiveFromTopicSpec;
 
 /**
  * @group functional
- * @retry 5
+ * @retry 10
  */
 class RdKafkaSendToAndReceiveFromTopicTest extends SendToAndReceiveFromTopicSpec
 {
