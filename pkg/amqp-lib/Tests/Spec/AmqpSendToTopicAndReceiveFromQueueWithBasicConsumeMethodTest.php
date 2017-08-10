b3139cd11a7093538bb548a87cd9c88c0815707a
Alexander Kozienko <pascalamsg@gmail.com>
2017-08-10 13:11:58 +0300

    fix tests

--- pkg/amqp-lib/Tests/Spec/AmqpSendToTopicAndReceiveFromQueueWithBasicConsumeMethodTest.php
+++ pkg/amqp-lib/Tests/Spec/AmqpSendToTopicAndReceiveFromQueueWithBasicConsumeMethodTest.php
@@ -26,48 +26,6 @@ class AmqpSendToTopicAndReceiveFromQueueWithBasicConsumeMethodTest extends SendT
         return $factory->createContext();
     }
 
-//    public function test()
-//    {
-//        $context = $this->createContext();
-//
-//        $topic = $context->createTopic('send_to_topic_and_receive_from_queue_spec_basic_consume');
-//        $topic->setType(AmqpTopic::TYPE_FANOUT);
-//        $topic->addFlag(AmqpTopic::FLAG_DURABLE);
-//
-//        try {
-//            $context->deleteTopic($topic);
-//        } catch (\Exception $e) {}
-//
-//        $context->declareTopic($topic);
-//
-//        $queue = $context->createQueue('send_to_topic_and_receive_from_queue_spec_basic_consume');
-//
-//        try {
-//            $context->deleteQueue($queue);
-//        } catch (\Exception $e) {}
-//
-//        $context->declareQueue($queue);
-//        $context->purgeQueue($queue);
-//
-//        $context->bind(new AmqpBind($topic, $queue));
-//
-//        $consumer = $context->createConsumer($queue);
-//
-//         // guard
-//        $this->assertNull($consumer->receiveNoWait());
-//
-//        $expectedBody = __CLASS__.time();
-//
-//        $context->createProducer()->send($topic, $context->createMessage($expectedBody));
-//
-//        $message = $consumer->receive(2000); // 2 sec
-//
-//        $this->assertInstanceOf(PsrMessage::class, $message);
-//        $consumer->acknowledge($message);
-//
-//        $this->assertSame($expectedBody, $message->getBody());
-//    }
-
     /**
      * {@inheritdoc}
      *
