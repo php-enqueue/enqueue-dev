f8abd0577e9491fc536f25a2223015ef601ed959
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-08-10 18:32:52 +0300

    fix tests.

---
+++ pkg/simple-client/Tests/fix_composer_json.php
@@ -0,0 +1,9 @@
+<?php
+
+copy(__DIR__.'/../composer.json', __DIR__.'/../composer.json.bak');
+
+$composerJson = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
+
+$composerJson['config']['platform']['ext-amqp'] = '1.7';
+
+file_put_contents(__DIR__.'/../composer.json', json_encode($composerJson, JSON_PRETTY_PRINT));
