c336b209f47573a0fe7bb286aa7a618220d11bdd
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-10-27 15:37:02 +0300

    fix typo.

--- bin/build-rabbitmq-image.sh
+++ bin/build-rabbitmq-image.sh
@@ -5,4 +5,4 @@ set -x
 
 (cd docker && docker build --rm --force-rm --no-cache --pull --squash --tag "enqueue/rabbitmq:latest" -f Dockerfile.rabbitmq .)
 (cd docker && docker login --username="$DOCKER_USER" --password="$DOCKER_PASSWORD")
-(cd docker && docker push "enqueue/rabbitmq-ssl:latest")
+(cd docker && docker push "enqueue/rabbitmq:latest")
