d873e5cad5fbbb23f88205eae25f889307cfab68
Thomas Ploch <thomas.ploch@flixbus.com>
2017-11-06 02:59:19 +0100

    Extract `json_*` to `functions.php` file and include with composer

--- pkg/rdkafka/JsonSerializer.php
+++ pkg/rdkafka/JsonSerializer.php
@@ -9,21 +9,14 @@ class JsonSerializer implements Serializer
      */
     public function toString(RdKafkaMessage $message)
     {
-        $json = json_encode([
-            'body' => $message->getBody(),
-            'properties' => $message->getProperties(),
-            'headers' => $message->getHeaders(),
-        ]);
-
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw new \InvalidArgumentException(sprintf(
-                'The malformed json given. Error %s and message %s',
-                json_last_error(),
-                json_last_error_msg()
-            ));
-        }
-
-        return $json;
+        return json_encode(
+            [
+                'body' => $message->getBody(),
+                'properties' => $message->getProperties(),
+                'headers' => $message->getHeaders(),
+            ],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 
     /**
@@ -31,14 +24,7 @@ class JsonSerializer implements Serializer
      */
     public function toMessage($string)
     {
-        $data = json_decode($string, true);
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw new \InvalidArgumentException(sprintf(
-                'The malformed json given. Error %s and message %s',
-                json_last_error(),
-                json_last_error_msg()
-            ));
-        }
+        $data = json_decode($string);
 
         return new RdKafkaMessage($data['body'], $data['properties'], $data['headers']);
     }
