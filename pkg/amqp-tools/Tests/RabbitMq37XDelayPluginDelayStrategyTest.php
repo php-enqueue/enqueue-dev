af6e2ed9c8fb4290bb35a8b1b070ba9653248649
Anton Sadovnikov <anton.sadovnikov@rambler-co.ru>
2018-06-14 00:45:17 +0300

    issues-455: fix after code review

--- pkg/amqp-tools/Tests/RabbitMq37XDelayPluginDelayStrategyTest.php
+++ pkg/amqp-tools/Tests/RabbitMq37XDelayPluginDelayStrategyTest.php
@@ -4,214 +4,22 @@ namespace Enqueue\AmqpTools\Tests;
 
 use Enqueue\AmqpTools\DelayStrategy;
 use Enqueue\AmqpTools\RabbitMq37XDelayPluginDelayStrategy;
-use Enqueue\Test\ClassExtensionTrait;
-use Interop\Amqp\AmqpBind;
-use Interop\Amqp\AmqpContext;
-use Interop\Amqp\AmqpDestination;
-use Interop\Amqp\AmqpProducer;
 use Interop\Amqp\Impl\AmqpMessage;
-use Interop\Amqp\Impl\AmqpQueue;
-use Interop\Amqp\Impl\AmqpTopic;
-use Interop\Queue\InvalidDestinationException;
-use Interop\Queue\PsrDestination;
-use Interop\Queue\PsrMessage;
-use PHPUnit\Framework\TestCase;
 
-class RabbitMq37XDelayPluginDelayStrategyTest extends TestCase
+class RabbitMq37XDelayPluginDelayStrategyTest extends RabbitMqDelayPluginDelayStrategyTest
 {
-    use ClassExtensionTrait;
-
     public function testShouldImplementDelayStrategyInterface()
     {
         $this->assertClassImplements(DelayStrategy::class, RabbitMq37XDelayPluginDelayStrategy::class);
     }
 
-    public function testShouldSendDelayedMessageToTopic()
+    protected function buildStrategy(): DelayStrategy
     {
-        $delayedTopic = new AmqpTopic('the-topic');
-        $delayedMessage = new AmqpMessage();
-
-        $producer = $this->createProducerMock();
-        $producer
-            ->expects($this->once())
-            ->method('send')
-            ->with($this->identicalTo($delayedTopic), $this->identicalTo($delayedMessage))
-        ;
-
-        $context = $this->createContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createTopic')
-            ->with($this->identicalTo('enqueue.the-topic.delayed'))
-            ->willReturn($delayedTopic)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('declareTopic')
-            ->with($this->identicalTo($delayedTopic))
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createProducer')
-            ->willReturn($producer)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createMessage')
-            ->with($this->identicalTo('the body'), $this->identicalTo(['key' => 'value']), $this->identicalTo(['hkey' => 'hvalue']))
-            ->willReturn($delayedMessage)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('bind')
-            ->with($this->isInstanceOf(AmqpBind::class))
-        ;
-
-        $message = new AmqpMessage('the body', ['key' => 'value'], ['hkey' => 'hvalue']);
-        $message->setRoutingKey('the-routing-key');
-
-        $dest = new AmqpTopic('the-topic');
-        $dest->setFlags(12345);
-
-        $strategy = new RabbitMq37XDelayPluginDelayStrategy();
-        $strategy->delayMessage($context, $dest, $message, 10000);
-
-        $this->assertSame(12345, $delayedTopic->getFlags());
-        $this->assertSame('x-delayed-message', $delayedTopic->getType());
-        $this->assertSame([
-            'x-delayed-type' => 'direct',
-        ], $delayedTopic->getArguments());
-
-        $this->assertSame(['x-delay' => 10000], $delayedMessage->getHeaders());
-        $this->assertSame('the-routing-key', $delayedMessage->getRoutingKey());
+        return new RabbitMq37XDelayPluginDelayStrategy();
     }
 
-    public function testShouldSendDelayedMessageToQueue()
+    protected function assertXDelay(AmqpMessage $delayedMessage, int $xDelay): void
     {
-        $delayedTopic = new AmqpTopic('the-topic');
-        $delayedMessage = new AmqpMessage();
-
-        $producer = $this->createProducerMock();
-        $producer
-            ->expects($this->once())
-            ->method('send')
-            ->with($this->identicalTo($delayedTopic), $this->identicalTo($delayedMessage))
-        ;
-
-        $context = $this->createContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createTopic')
-            ->with($this->identicalTo('enqueue.queue.delayed'))
-            ->willReturn($delayedTopic)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('declareTopic')
-            ->with($this->identicalTo($delayedTopic))
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createProducer')
-            ->willReturn($producer)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createMessage')
-            ->with($this->identicalTo('the body'), $this->identicalTo(['key' => 'value']), $this->identicalTo(['hkey' => 'hvalue']))
-            ->willReturn($delayedMessage)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('bind')
-            ->with($this->isInstanceOf(AmqpBind::class))
-        ;
-
-        $message = new AmqpMessage('the body', ['key' => 'value'], ['hkey' => 'hvalue']);
-        $message->setRoutingKey('the-routing-key');
-
-        $dest = new AmqpQueue('the-queue');
-
-        $strategy = new RabbitMq37XDelayPluginDelayStrategy();
-        $strategy->delayMessage($context, $dest, $message, 10000);
-
-        $this->assertSame(AmqpQueue::FLAG_DURABLE, $delayedTopic->getFlags());
-        $this->assertSame('x-delayed-message', $delayedTopic->getType());
-        $this->assertSame([
-            'x-delayed-type' => 'direct',
-        ], $delayedTopic->getArguments());
-
-        $this->assertSame(['x-delay' => 10000], $delayedMessage->getHeaders());
-        $this->assertSame('the-queue', $delayedMessage->getRoutingKey());
-    }
-
-    public function testShouldThrowExceptionIfInvalidDestination()
-    {
-        $delayedMessage = new AmqpMessage();
-
-        $context = $this->createContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createMessage')
-            ->willReturn($delayedMessage)
-        ;
-
-        $strategy = new RabbitMq37XDelayPluginDelayStrategy();
-
-        $this->expectException(InvalidDestinationException::class);
-        $this->expectExceptionMessage('The destination must be an instance of Interop\Amqp\AmqpTopic|Interop\Amqp\AmqpQueue but got');
-
-        $strategy->delayMessage($context, $this->createMock(AmqpDestination::class), new AmqpMessage(), 10000);
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|AmqpContext
-     */
-    private function createContextMock()
-    {
-        return $this->createMock(AmqpContext::class);
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|TestProducer
-     */
-    private function createProducerMock()
-    {
-        return $this->createMock(TestProducer::class);
-    }
-}
-
-class TestProducer implements AmqpProducer, DelayStrategy
-{
-    public function delayMessage(AmqpContext $context, AmqpDestination $dest, \Interop\Amqp\AmqpMessage $message, $delayMsec)
-    {
-    }
-
-    public function send(PsrDestination $destination, PsrMessage $message)
-    {
-    }
-
-    public function setDeliveryDelay($deliveryDelay)
-    {
-    }
-
-    public function getDeliveryDelay()
-    {
-    }
-
-    public function setPriority($priority)
-    {
-    }
-
-    public function getPriority()
-    {
-    }
-
-    public function setTimeToLive($timeToLive)
-    {
-    }
-
-    public function getTimeToLive()
-    {
+        $this->assertSame(['x-delay' => $xDelay], $delayedMessage->getHeaders());
     }
 }
