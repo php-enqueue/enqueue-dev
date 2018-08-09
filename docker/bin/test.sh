a1d81863883bd40462282a49a978190936da2b31
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-08-09 15:20:52 +0300

    fixes

--- docker/bin/test.sh
+++ docker/bin/test.sh
@@ -40,6 +40,8 @@ waitForService mongo 27017 50
 waitForService localstack 4576 50
 
 php docker/bin/refresh-mysql-database.php
+php pkg/job-queue/Tests/Functional/app/console doctrine:database:create --if-not-exists
+php pkg/job-queue/Tests/Functional/app/console doctrine:schema:update --force
 
 #php pkg/enqueue-bundle/Tests/Functional/app/console.php config:dump-reference  enqueue
 bin/phpunit "$@"
