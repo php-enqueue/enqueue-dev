640024d490670445f94bae48053826ac6ed20a66
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-08-13 15:37:49 +0300

    [BC break][dbal] Convert EnqueueClient expire to Dbal time to live and back.

--- pkg/dbal/Client/DbalDriver.php
+++ pkg/dbal/Client/DbalDriver.php
@@ -71,8 +71,12 @@ class DbalDriver implements DriverInterface
         $transportMessage->setProperties($properties);
         $transportMessage->setMessageId($message->getMessageId());
         $transportMessage->setTimestamp($message->getTimestamp());
-        $transportMessage->setDeliveryDelay($message->getDelay());
-        $transportMessage->setTimeToLive($message->getExpire());
+
+        $delay = $message->getDelay();
+        $transportMessage->setDeliveryDelay((null === $delay) ? null : ($delay * 1000));
+
+        $timeToLive = $message->getExpire();
+        $transportMessage->setTimeToLive((null === $timeToLive) ? null : ($timeToLive * 1000));
         $transportMessage->setReplyTo($message->getReplyTo());
         $transportMessage->setCorrelationId($message->getCorrelationId());
         if (array_key_exists($message->getPriority(), self::$priorityMap)) {
@@ -98,9 +102,12 @@ class DbalDriver implements DriverInterface
         $clientMessage->setContentType($message->getHeader('content_type'));
         $clientMessage->setMessageId($message->getMessageId());
         $clientMessage->setTimestamp($message->getTimestamp());
+
         $timeToLive = $message->getTimeToLive();
-        $clientMessage->setExpire((null === $timeToLive) ? null : (int) round($timeToLive));
-        $clientMessage->setDelay($message->getDeliveryDelay());
+        $clientMessage->setExpire((null === $timeToLive) ? null : (int) ($timeToLive / 1000));
+
+        $delay = $message->getDeliveryDelay();
+        $clientMessage->setDelay((null === $delay) ? null : (int) ($delay / 1000));
         $clientMessage->setReplyTo($message->getReplyTo());
         $clientMessage->setCorrelationId($message->getCorrelationId());
 
