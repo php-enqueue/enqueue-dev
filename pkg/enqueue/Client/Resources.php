1c93c89942813d092a55ad9f598ea6ec658db5c5
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-08-20 16:19:12 +0300

    add tests

--- pkg/enqueue/Client/Resources.php
+++ pkg/enqueue/Client/Resources.php
@@ -50,19 +50,63 @@ final class Resources
         if (null === self::$knownDrivers) {
             $map = [];
 
-            $map[AmqpDriver::class] = ['supportedSchemeProtocols' => ['amqp'], 'requiredSchemeExtensions' => []];
-            $map[RabbitMqDriver::class] = ['supportedSchemeProtocols' => ['amqp'], 'requiredSchemeExtensions' => ['rabbitmq']];
-            $map[FsDriver::class] = ['supportedSchemeProtocols' => ['file'], 'requiredSchemeExtensions' => []];
-            $map[NullDriver::class] = ['supportedSchemeProtocols' => ['null'], 'requiredSchemeExtensions' => []];
-            $map[GpsDriver::class] = ['supportedSchemeProtocols' => ['gps'], 'requiredSchemeExtensions' => []];
-            $map[RedisDriver::class] = ['supportedSchemeProtocols' => ['redis'], 'requiredSchemeExtensions' => []];
-            $map[SqsDriver::class] = ['supportedSchemeProtocols' => ['sqs'], 'requiredSchemeExtensions' => []];
-            $map[StompDriver::class] = ['supportedSchemeProtocols' => ['stomp'], 'requiredSchemeExtensions' => []];
-            $map[RabbitMqStompDriver::class] = ['supportedSchemeProtocols' => ['stomp'], 'requiredSchemeExtensions' => ['rabbitmq']];
-            $map[RdKafkaDriver::class] = ['supportedSchemeProtocols' => ['kafka', 'rdkafka'], 'requiredSchemeExtensions' => []];
-            $map[MongodbDriver::class] = ['supportedSchemeProtocols' => ['mongodb'], 'requiredSchemeExtensions' => []];
+            $map[AmqpDriver::class] = [
+                'schemes' => ['amqp'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/enqueue',
+            ];
+            $map[RabbitMqDriver::class] = [
+                'schemes' => ['amqp'],
+                'requiredSchemeExtensions' => ['rabbitmq'],
+                'package' => 'enqueue/enqueue',
+            ];
+            $map[FsDriver::class] = [
+                'schemes' => ['file'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/fs',
+            ];
+            $map[NullDriver::class] = [
+                'schemes' => ['null'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/null',
+            ];
+            $map[GpsDriver::class] = [
+                'schemes' => ['gps'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/gps',
+            ];
+            $map[RedisDriver::class] = [
+                'schemes' => ['redis'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/redis',
+            ];
+            $map[SqsDriver::class] = [
+                'schemes' => ['sqs'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/sqs',
+            ];
+            $map[StompDriver::class] = [
+                'schemes' => ['stomp'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/stomp',
+            ];
+            $map[RabbitMqStompDriver::class] = [
+                'schemes' => ['stomp'],
+                'requiredSchemeExtensions' => ['rabbitmq'],
+                'package' => 'enqueue/stomp',
+            ];
+            $map[RdKafkaDriver::class] = [
+                'schemes' => ['kafka', 'rdkafka'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/rdkafka',
+            ];
+            $map[MongodbDriver::class] = [
+                'schemes' => ['mongodb'],
+                'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/mongodb',
+            ];
             $map[DbalDriver::class] = [
-                'supportedSchemeProtocols' => [
+                'schemes' => [
                     'db2',
                     'ibm_db2',
                     'mssql',
@@ -78,6 +122,7 @@ final class Resources
                     'pdo_sqlite',
                 ],
                 'requiredSchemeExtensions' => [],
+                'package' => 'enqueue/dbal',
             ];
 
             self::$knownDrivers = $map;
@@ -86,24 +131,26 @@ final class Resources
         return self::$knownDrivers;
     }
 
-    public static function addDriver(string $driverClass, array $schemes, array $requiredExtensions): void
+    public static function addDriver(string $driverClass, array $schemes, array $requiredExtensions, string $package): void
     {
         if (class_exists($driverClass)) {
-            throw new \InvalidArgumentException(sprintf('The driver class "%s" does not exist.', $driverClass));
-        }
-
-        if (false == (new \ReflectionClass($driverClass))->implementsInterface(DriverInterface::class)) {
-            throw new \InvalidArgumentException(sprintf('The driver class "%s" must implement "%s" interface.', $driverClass, DriverInterface::class));
+            if (false == (new \ReflectionClass($driverClass))->implementsInterface(DriverInterface::class)) {
+                throw new \InvalidArgumentException(sprintf('The driver class "%s" must implement "%s" interface.', $driverClass, DriverInterface::class));
+            }
         }
 
         if (empty($schemes)) {
-            throw new \InvalidArgumentException('Schemes could not be empty');
+            throw new \InvalidArgumentException('Schemes could not be empty.');
         }
         if (empty($package)) {
-            throw new \InvalidArgumentException('Package name could not be empty');
+            throw new \InvalidArgumentException('Package name could not be empty.');
         }
 
         self::getKnownDrivers();
-        self::$knownDrivers[$driverClass] = ['supportedSchemeProtocols' => $schemes, 'requiredSchemeExtensions' => $requiredExtensions];
+        self::$knownDrivers[$driverClass] = [
+            'schemes' => $schemes,
+            'requiredSchemeExtensions' => $requiredExtensions,
+            'package' => $package,
+        ];
     }
 }
--- pkg/enqueue/Tests/Client/ResourcesTest.php
+++ pkg/enqueue/Tests/Client/ResourcesTest.php
@@ -2,7 +2,10 @@
 
 namespace Enqueue\Tests\Client;
 
+use Enqueue\Client\Amqp\RabbitMqDriver;
+use Enqueue\Client\DriverInterface;
 use Enqueue\Client\Resources;
+use Enqueue\Redis\Client\RedisDriver;
 use PHPUnit\Framework\TestCase;
 
 class ResourcesTest extends TestCase
@@ -19,5 +22,125 @@ class ResourcesTest extends TestCase
         $rc = new \ReflectionClass(Resources::class);
 
         $this->assertTrue($rc->getConstructor()->isPrivate());
+    }
+
+    public function testShouldGetAvailableDriverInExpectedFormat()
+    {
+        $availableDrivers = Resources::getAvailableDrivers();
+
+        $this->assertInternalType('array', $availableDrivers);
+        $this->assertArrayHasKey(RedisDriver::class, $availableDrivers);
+
+        $driverInfo = $availableDrivers[RedisDriver::class];
+        $this->assertArrayHasKey('schemes', $driverInfo);
+        $this->assertSame(['redis'], $driverInfo['schemes']);
+
+        $this->assertArrayHasKey('requiredSchemeExtensions', $driverInfo);
+        $this->assertSame([], $driverInfo['requiredSchemeExtensions']);
+
+        $this->assertArrayHasKey('package', $driverInfo);
+        $this->assertSame('enqueue/redis', $driverInfo['package']);
+    }
+
+    public function testShouldGetAvailableDriverWithRequiredExtensionInExpectedFormat()
+    {
+        $availableDrivers = Resources::getAvailableDrivers();
+
+        $this->assertInternalType('array', $availableDrivers);
+        $this->assertArrayHasKey(RabbitMqDriver::class, $availableDrivers);
+
+        $driverInfo = $availableDrivers[RabbitMqDriver::class];
+        $this->assertArrayHasKey('schemes', $driverInfo);
+        $this->assertSame(['amqp'], $driverInfo['schemes']);
+
+        $this->assertArrayHasKey('requiredSchemeExtensions', $driverInfo);
+        $this->assertSame(['rabbitmq'], $driverInfo['requiredSchemeExtensions']);
+
+        $this->assertArrayHasKey('package', $driverInfo);
+        $this->assertSame('enqueue/enqueue', $driverInfo['package']);
+    }
+
+    public function testShouldGetKnownDriversInExpectedFormat()
+    {
+        $knownDrivers = Resources::getKnownDrivers();
+
+        $this->assertInternalType('array', $knownDrivers);
+        $this->assertArrayHasKey(RedisDriver::class, $knownDrivers);
+
+        $driverInfo = $knownDrivers[RedisDriver::class];
+        $this->assertArrayHasKey('schemes', $driverInfo);
+        $this->assertSame(['redis'], $driverInfo['schemes']);
+
+        $this->assertArrayHasKey('requiredSchemeExtensions', $driverInfo);
+        $this->assertSame([], $driverInfo['requiredSchemeExtensions']);
+    }
+
+    public function testThrowsIfDriverClassNotImplementDriverFactoryInterfaceOnAddDriver()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The driver class "stdClass" must implement "Enqueue\Client\DriverInterface" interface.');
+
+        Resources::addDriver(\stdClass::class, [], [], 'foo');
+    }
+
+    public function testThrowsIfNoSchemesProvidedOnAddDriver()
+    {
+        $driverClass = $this->getMockClass(DriverInterface::class);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Schemes could not be empty.');
+
+        Resources::addDriver($driverClass, [], [], 'foo');
+    }
+
+    public function testThrowsIfNoPackageProvidedOnAddDriver()
+    {
+        $driverClass = $this->getMockClass(DriverInterface::class);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Package name could not be empty.');
+
+        Resources::addDriver($driverClass, ['foo'], [], '');
+    }
+
+    public function testShouldAllowRegisterDriverThatIsNotInstalled()
+    {
+        Resources::addDriver('theDriverClass', ['foo'], ['barExtension'], 'foo');
+
+        $knownDrivers = Resources::getKnownDrivers();
+        $this->assertInternalType('array', $knownDrivers);
+        $this->assertArrayHasKey('theDriverClass', $knownDrivers);
+
+        $availableDrivers = Resources::getAvailableDrivers();
+
+        $this->assertInternalType('array', $availableDrivers);
+        $this->assertArrayNotHasKey('theDriverClass', $availableDrivers);
+    }
+
+    public function testShouldAllowGetPreviouslyRegisteredDriver()
+    {
+        $driverClass = $this->getMockClass(DriverInterface::class);
+
+        Resources::addDriver(
+            $driverClass,
+            ['fooscheme', 'barscheme'],
+            ['fooextension', 'barextension'],
+            'foo/bar'
+        );
+
+        $availableDrivers = Resources::getAvailableDrivers();
+
+        $this->assertInternalType('array', $availableDrivers);
+        $this->assertArrayHasKey($driverClass, $availableDrivers);
+
+        $driverInfo = $availableDrivers[$driverClass];
+        $this->assertArrayHasKey('schemes', $driverInfo);
+        $this->assertSame(['fooscheme', 'barscheme'], $driverInfo['schemes']);
+
+        $this->assertArrayHasKey('requiredSchemeExtensions', $driverInfo);
+        $this->assertSame(['fooextension', 'barextension'], $driverInfo['requiredSchemeExtensions']);
+
+        $this->assertArrayHasKey('package', $driverInfo);
+        $this->assertSame('foo/bar', $driverInfo['package']);
     }
 }
