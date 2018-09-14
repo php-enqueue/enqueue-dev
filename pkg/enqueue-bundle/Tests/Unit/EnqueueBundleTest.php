cf03113f381a00e70e1687c4a97889475813721d
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-09-14 16:35:57 +0300

    remove tests.

--- pkg/enqueue-bundle/Tests/Unit/EnqueueBundleTest.php
+++ pkg/enqueue-bundle/Tests/Unit/EnqueueBundleTest.php
@@ -2,15 +2,9 @@
 
 namespace Enqueue\Bundle\Tests\Unit;
 
-use Enqueue\Bundle\DependencyInjection\Compiler\BuildClientExtensionsPass;
-use Enqueue\Bundle\DependencyInjection\Compiler\BuildConsumptionExtensionsPass;
-use Enqueue\Bundle\DependencyInjection\Compiler\BuildProcessorRegistryPass;
-use Enqueue\Bundle\DependencyInjection\Compiler\BuildQueueMetaRegistryPass;
-use Enqueue\Bundle\DependencyInjection\Compiler\BuildTopicMetaSubscribersPass;
 use Enqueue\Bundle\EnqueueBundle;
 use Enqueue\Test\ClassExtensionTrait;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EnqueueBundleTest extends TestCase
@@ -25,38 +19,5 @@ class EnqueueBundleTest extends TestCase
     public function testCouldBeConstructedWithoutAnyArguments()
     {
         new EnqueueBundle();
-    }
-
-    public function testShouldRegisterExpectedCompilerPasses()
-    {
-        $container = $this->createMock(ContainerBuilder::class);
-        $container
-            ->expects($this->at(0))
-            ->method('addCompilerPass')
-            ->with($this->isInstanceOf(BuildConsumptionExtensionsPass::class))
-        ;
-        $container
-            ->expects($this->at(2))
-            ->method('addCompilerPass')
-            ->with($this->isInstanceOf(BuildProcessorRegistryPass::class))
-        ;
-        $container
-            ->expects($this->at(3))
-            ->method('addCompilerPass')
-            ->with($this->isInstanceOf(BuildTopicMetaSubscribersPass::class))
-        ;
-        $container
-            ->expects($this->at(4))
-            ->method('addCompilerPass')
-            ->with($this->isInstanceOf(BuildQueueMetaRegistryPass::class))
-        ;
-        $container
-            ->expects($this->at(5))
-            ->method('addCompilerPass')
-            ->with($this->isInstanceOf(BuildClientExtensionsPass::class))
-        ;
-
-        $bundle = new EnqueueBundle();
-        $bundle->build($container);
     }
 }
