ca7b0b766b104a922218b4970c4ffd5d0cfddf04
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2018-09-18 08:00:27 +0300

    [client] Extend DbalDriver from GenericDriver.

--- pkg/enqueue/Client/Driver/DbalDriver.php
+++ pkg/enqueue/Client/Driver/DbalDriver.php
@@ -2,155 +2,15 @@
 
 namespace Enqueue\Client\Driver;
 
-use Enqueue\Client\Config;
-use Enqueue\Client\DriverInterface;
-use Enqueue\Client\Message;
-use Enqueue\Client\MessagePriority;
-use Enqueue\Client\Meta\QueueMetaRegistry;
 use Enqueue\Dbal\DbalContext;
-use Enqueue\Dbal\DbalDestination;
-use Enqueue\Dbal\DbalMessage;
-use Interop\Queue\PsrMessage;
-use Interop\Queue\PsrQueue;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 
-class DbalDriver implements DriverInterface
+/**
+ * @method DbalContext getContext
+ */
+class DbalDriver extends GenericDriver
 {
-    /**
-     * @var DbalContext
-     */
-    private $context;
-
-    /**
-     * @var Config
-     */
-    private $config;
-
-    /**
-     * @var QueueMetaRegistry
-     */
-    private $queueMetaRegistry;
-
-    /**
-     * @var array
-     */
-    private static $priorityMap = [
-        MessagePriority::VERY_LOW => 0,
-        MessagePriority::LOW => 1,
-        MessagePriority::NORMAL => 2,
-        MessagePriority::HIGH => 3,
-        MessagePriority::VERY_HIGH => 4,
-    ];
-
-    public function __construct(DbalContext $context, Config $config, QueueMetaRegistry $queueMetaRegistry)
-    {
-        $this->context = $context;
-        $this->config = $config;
-        $this->queueMetaRegistry = $queueMetaRegistry;
-    }
-
-    /**
-     * @return DbalMessage
-     */
-    public function createTransportMessage(Message $message): PsrMessage
-    {
-        $properties = $message->getProperties();
-
-        $headers = $message->getHeaders();
-        $headers['content_type'] = $message->getContentType();
-
-        $transportMessage = $this->context->createMessage();
-        $transportMessage->setBody($message->getBody());
-        $transportMessage->setHeaders($headers);
-        $transportMessage->setProperties($properties);
-        $transportMessage->setMessageId($message->getMessageId());
-        $transportMessage->setTimestamp($message->getTimestamp());
-
-        $delay = $message->getDelay();
-        $transportMessage->setDeliveryDelay((null === $delay) ? null : ($delay * 1000));
-
-        $timeToLive = $message->getExpire();
-        $transportMessage->setTimeToLive((null === $timeToLive) ? null : ($timeToLive * 1000));
-        $transportMessage->setReplyTo($message->getReplyTo());
-        $transportMessage->setCorrelationId($message->getCorrelationId());
-        if (array_key_exists($message->getPriority(), self::$priorityMap)) {
-            $transportMessage->setPriority(self::$priorityMap[$message->getPriority()]);
-        }
-
-        return $transportMessage;
-    }
-
-    /**
-     * @param DbalMessage $message
-     */
-    public function createClientMessage(PsrMessage $message): Message
-    {
-        $clientMessage = new Message();
-
-        $clientMessage->setBody($message->getBody());
-        $clientMessage->setHeaders($message->getHeaders());
-        $clientMessage->setProperties($message->getProperties());
-
-        $clientMessage->setContentType($message->getHeader('content_type'));
-        $clientMessage->setMessageId($message->getMessageId());
-        $clientMessage->setTimestamp($message->getTimestamp());
-
-        $timeToLive = $message->getTimeToLive();
-        $clientMessage->setExpire((null === $timeToLive) ? null : (int) ($timeToLive / 1000));
-
-        $delay = $message->getDeliveryDelay();
-        $clientMessage->setDelay((null === $delay) ? null : (int) ($delay / 1000));
-        $clientMessage->setReplyTo($message->getReplyTo());
-        $clientMessage->setCorrelationId($message->getCorrelationId());
-
-        $priorityMap = array_flip(self::$priorityMap);
-        $priority = array_key_exists($message->getPriority(), $priorityMap) ?
-            $priorityMap[$message->getPriority()] :
-            MessagePriority::NORMAL;
-        $clientMessage->setPriority($priority);
-
-        return $clientMessage;
-    }
-
-    public function sendToRouter(Message $message): void
-    {
-        if (false == $message->getProperty(Config::PARAMETER_TOPIC_NAME)) {
-            throw new \LogicException('Topic name parameter is required but is not set');
-        }
-
-        $queue = $this->createQueue($this->config->getRouterQueueName());
-        $transportMessage = $this->createTransportMessage($message);
-
-        $this->context->createProducer()->send($queue, $transportMessage);
-    }
-
-    public function sendToProcessor(Message $message): void
-    {
-        if (false == $message->getProperty(Config::PARAMETER_PROCESSOR_NAME)) {
-            throw new \LogicException('Processor name parameter is required but is not set');
-        }
-
-        if (false == $queueName = $message->getProperty(Config::PARAMETER_PROCESSOR_QUEUE_NAME)) {
-            throw new \LogicException('Queue name parameter is required but is not set');
-        }
-
-        $transportMessage = $this->createTransportMessage($message);
-        $destination = $this->createQueue($queueName);
-
-        $this->context->createProducer()->send($destination, $transportMessage);
-    }
-
-    /**
-     * @return DbalDestination
-     */
-    public function createQueue(string $queueName): PsrQueue
-    {
-        $transportName = $this->queueMetaRegistry->getQueueMeta($queueName)->getTransportName();
-
-        return $this->context->createQueue($transportName);
-    }
-
     public function setupBroker(LoggerInterface $logger = null): void
     {
         $logger = $logger ?: new NullLogger();
@@ -158,17 +18,7 @@ class DbalDriver implements DriverInterface
             $logger->debug(sprintf('[DbalDriver] '.$text, ...$args));
         };
 
-        $log('Creating database table: "%s"', $this->context->getTableName());
-        $this->context->createDataBaseTable();
-    }
-
-    public function getConfig(): Config
-    {
-        return $this->config;
-    }
-
-    public static function getPriorityMap(): array
-    {
-        return self::$priorityMap;
+        $log('Creating database table: "%s"', $this->getContext()->getTableName());
+        $this->getContext()->createDataBaseTable();
     }
 }
--- pkg/enqueue/Tests/Client/Driver/DbalDriverTest.php
+++ pkg/enqueue/Tests/Client/Driver/DbalDriverTest.php
@@ -2,304 +2,39 @@
 
 namespace Enqueue\Tests\Client\Driver;
 
-use Enqueue\Client\Config;
 use Enqueue\Client\Driver\DbalDriver;
+use Enqueue\Client\Driver\GenericDriver;
 use Enqueue\Client\DriverInterface;
-use Enqueue\Client\Message;
-use Enqueue\Client\MessagePriority;
-use Enqueue\Client\Meta\QueueMetaRegistry;
+use Enqueue\Client\RouteCollection;
 use Enqueue\Dbal\DbalContext;
 use Enqueue\Dbal\DbalDestination;
 use Enqueue\Dbal\DbalMessage;
+use Enqueue\Dbal\DbalProducer;
 use Enqueue\Test\ClassExtensionTrait;
+use Interop\Queue\PsrContext;
+use Interop\Queue\PsrMessage;
 use Interop\Queue\PsrProducer;
+use Interop\Queue\PsrQueue;
+use Interop\Queue\PsrTopic;
 
 class DbalDriverTest extends \PHPUnit_Framework_TestCase
 {
     use ClassExtensionTrait;
+    use GenericDriverTestsTrait;
 
     public function testShouldImplementsDriverInterface()
     {
         $this->assertClassImplements(DriverInterface::class, DbalDriver::class);
     }
 
-    public function testCouldBeConstructedWithRequiredArguments()
+    public function testShouldBeSubClassOfGenericDriver()
     {
-        new DbalDriver(
-            $this->createPsrContextMock(),
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-    }
-
-    public function testShouldReturnConfigObject()
-    {
-        $config = $this->createDummyConfig();
-
-        $driver = new DbalDriver(
-            $this->createPsrContextMock(),
-            $config,
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $this->assertSame($config, $driver->getConfig());
-    }
-
-    public function testShouldCreateAndReturnQueueInstance()
-    {
-        $expectedQueue = new DbalDestination('aName');
-
-        $context = $this->createPsrContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createQueue')
-            ->with('aprefix.afooqueue')
-            ->willReturn($expectedQueue)
-        ;
-
-        $driver = new DbalDriver($context, $this->createDummyConfig(), $this->createDummyQueueMetaRegistry());
-
-        $queue = $driver->createQueue('aFooQueue');
-
-        $this->assertSame($expectedQueue, $queue);
-    }
-
-    public function testShouldCreateAndReturnQueueInstanceWithHardcodedTransportName()
-    {
-        $expectedQueue = new DbalDestination('aName');
-
-        $context = $this->createPsrContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createQueue')
-            ->with('aBarQueue')
-            ->willReturn($expectedQueue)
-        ;
-
-        $driver = new DbalDriver($context, $this->createDummyConfig(), $this->createDummyQueueMetaRegistry());
-
-        $queue = $driver->createQueue('aBarQueue');
-
-        $this->assertSame($expectedQueue, $queue);
-    }
-
-    public function testShouldConvertTransportMessageToClientMessage()
-    {
-        $transportMessage = new DbalMessage();
-        $transportMessage->setBody('body');
-        $transportMessage->setHeaders(['hkey' => 'hval']);
-        $transportMessage->setProperties(['key' => 'val']);
-        $transportMessage->setHeader('content_type', 'ContentType');
-        $transportMessage->setMessageId('MessageId');
-        $transportMessage->setTimestamp(1000);
-        $transportMessage->setPriority(2);
-        $transportMessage->setDeliveryDelay(12345);
-        $transportMessage->setTimeToLive(67890);
-
-        $driver = new DbalDriver(
-            $this->createPsrContextMock(),
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $clientMessage = $driver->createClientMessage($transportMessage);
-
-        $this->assertInstanceOf(Message::class, $clientMessage);
-        $this->assertSame('body', $clientMessage->getBody());
-        $this->assertSame([
-            'hkey' => 'hval',
-            'content_type' => 'ContentType',
-            'message_id' => 'MessageId',
-            'timestamp' => 1000,
-        ], $clientMessage->getHeaders());
-        $this->assertSame([
-            'key' => 'val',
-        ], $clientMessage->getProperties());
-        $this->assertSame('MessageId', $clientMessage->getMessageId());
-        $this->assertSame('ContentType', $clientMessage->getContentType());
-        $this->assertSame(1000, $clientMessage->getTimestamp());
-        $this->assertSame(12, $clientMessage->getDelay());
-        $this->assertSame(67, $clientMessage->getExpire());
-        $this->assertSame(MessagePriority::NORMAL, $clientMessage->getPriority());
-    }
-
-    public function testShouldConvertClientMessageToTransportMessage()
-    {
-        $clientMessage = new Message();
-        $clientMessage->setBody('body');
-        $clientMessage->setHeaders(['hkey' => 'hval']);
-        $clientMessage->setProperties(['key' => 'val']);
-        $clientMessage->setContentType('ContentType');
-        $clientMessage->setExpire(123);
-        $clientMessage->setPriority(MessagePriority::VERY_HIGH);
-        $clientMessage->setMessageId('MessageId');
-        $clientMessage->setTimestamp(1000);
-        $clientMessage->setDelay(23);
-
-        $context = $this->createPsrContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createMessage')
-            ->willReturn(new DbalMessage())
-        ;
-
-        $driver = new DbalDriver(
-            $context,
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $transportMessage = $driver->createTransportMessage($clientMessage);
-
-        $this->assertInstanceOf(DbalMessage::class, $transportMessage);
-        $this->assertSame('body', $transportMessage->getBody());
-        $this->assertSame([
-            'hkey' => 'hval',
-            'content_type' => 'ContentType',
-            'message_id' => 'MessageId',
-            'timestamp' => 1000,
-            'reply_to' => null,
-            'correlation_id' => null,
-        ], $transportMessage->getHeaders());
-        $this->assertSame([
-            'key' => 'val',
-        ], $transportMessage->getProperties());
-        $this->assertSame(123000, $transportMessage->getTimeToLive());
-        $this->assertSame('MessageId', $transportMessage->getMessageId());
-        $this->assertSame(1000, $transportMessage->getTimestamp());
-        $this->assertSame(23000, $transportMessage->getDeliveryDelay());
-    }
-
-    public function testShouldSendMessageToRouter()
-    {
-        $topic = new DbalDestination('queue-name');
-        $transportMessage = new DbalMessage();
-
-        $producer = $this->createPsrProducerMock();
-        $producer
-            ->expects($this->once())
-            ->method('send')
-            ->with($this->identicalTo($topic), $this->identicalTo($transportMessage))
-        ;
-        $context = $this->createPsrContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createQueue')
-            ->with('aprefix.default')
-            ->willReturn($topic)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createProducer')
-            ->willReturn($producer)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createMessage')
-            ->willReturn($transportMessage)
-        ;
-
-        $driver = new DbalDriver(
-            $context,
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $message = new Message();
-        $message->setProperty(Config::PARAMETER_TOPIC_NAME, 'topic');
-
-        $driver->sendToRouter($message);
-    }
-
-    public function testShouldThrowExceptionIfTopicParameterIsNotSet()
-    {
-        $driver = new DbalDriver(
-            $this->createPsrContextMock(),
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $this->expectException(\LogicException::class);
-        $this->expectExceptionMessage('Topic name parameter is required but is not set');
-
-        $driver->sendToRouter(new Message());
-    }
-
-    public function testShouldSendMessageToProcessor()
-    {
-        $queue = new DbalDestination('queue-name');
-        $transportMessage = new DbalMessage();
-
-        $producer = $this->createPsrProducerMock();
-        $producer
-            ->expects($this->once())
-            ->method('send')
-            ->with($this->identicalTo($queue), $this->identicalTo($transportMessage))
-        ;
-        $context = $this->createPsrContextMock();
-        $context
-            ->expects($this->once())
-            ->method('createQueue')
-            ->willReturn($queue)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createProducer')
-            ->willReturn($producer)
-        ;
-        $context
-            ->expects($this->once())
-            ->method('createMessage')
-            ->willReturn($transportMessage)
-        ;
-
-        $driver = new DbalDriver(
-            $context,
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $message = new Message();
-        $message->setProperty(Config::PARAMETER_PROCESSOR_NAME, 'processor');
-        $message->setProperty(Config::PARAMETER_PROCESSOR_QUEUE_NAME, 'aFooQueue');
-
-        $driver->sendToProcessor($message);
-    }
-
-    public function testShouldThrowExceptionIfProcessorNameParameterIsNotSet()
-    {
-        $driver = new DbalDriver(
-            $this->createPsrContextMock(),
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $this->expectException(\LogicException::class);
-        $this->expectExceptionMessage('Processor name parameter is required but is not set');
-
-        $driver->sendToProcessor(new Message());
-    }
-
-    public function testShouldThrowExceptionIfProcessorQueueNameParameterIsNotSet()
-    {
-        $driver = new DbalDriver(
-            $this->createPsrContextMock(),
-            $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
-        );
-
-        $this->expectException(\LogicException::class);
-        $this->expectExceptionMessage('Queue name parameter is required but is not set');
-
-        $message = new Message();
-        $message->setProperty(Config::PARAMETER_PROCESSOR_NAME, 'processor');
-
-        $driver->sendToProcessor($message);
+        $this->assertClassExtends(GenericDriver::class, DbalDriver::class);
     }
 
     public function testShouldSetupBroker()
     {
-        $context = $this->createPsrContextMock();
+        $context = $this->createContextMock();
         $context
             ->expects($this->once())
             ->method('getTableName')
@@ -312,46 +47,54 @@ class DbalDriverTest extends \PHPUnit_Framework_TestCase
         $driver = new DbalDriver(
             $context,
             $this->createDummyConfig(),
-            $this->createDummyQueueMetaRegistry()
+            new RouteCollection([])
         );
 
         $driver->setupBroker();
     }
 
+    protected function createDriver(...$args): DriverInterface
+    {
+        return new DbalDriver(...$args);
+    }
+
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|DbalContext
+     * @return DbalContext
      */
-    private function createPsrContextMock()
+    protected function createContextMock(): PsrContext
     {
         return $this->createMock(DbalContext::class);
     }
 
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|PsrProducer
+     * @return DbalProducer
      */
-    private function createPsrProducerMock()
+    protected function createProducerMock(): PsrProducer
     {
-        return $this->createMock(PsrProducer::class);
+        return $this->createMock(DbalProducer::class);
     }
 
     /**
-     * @return QueueMetaRegistry
+     * @return DbalDestination
      */
-    private function createDummyQueueMetaRegistry()
+    protected function createQueue(string $name): PsrQueue
     {
-        $registry = new QueueMetaRegistry($this->createDummyConfig(), []);
-        $registry->add('default');
-        $registry->add('aFooQueue');
-        $registry->add('aBarQueue', 'aBarQueue');
-
-        return $registry;
+        return new DbalDestination(new \SplFileInfo($name));
     }
 
     /**
-     * @return Config
+     * @return DbalDestination
      */
-    private function createDummyConfig()
+    protected function createTopic(string $name): PsrTopic
     {
-        return Config::create('aPrefix');
+        return new DbalDestination(new \SplFileInfo($name));
+    }
+
+    /**
+     * @return DbalMessage
+     */
+    protected function createMessage(): PsrMessage
+    {
+        return new DbalMessage();
     }
 }
