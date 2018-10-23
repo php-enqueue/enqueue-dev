715bb7f8d889746a9f9d177a52bdb6accd9e9a9e
Alexander Kozienko <a.s.kozienko@gmail.com>
2018-10-23 15:02:33 +0300

    wamp

--- docker/bin/test.sh
+++ docker/bin/test.sh
@@ -37,6 +37,7 @@ waitForService beanstalkd 11300 50
 waitForService gearmand 4730 50
 waitForService kafka 9092 50
 waitForService mongo 27017 50
+waitForService thruway 9090 50
 waitForService localstack 4576 50
 
 php docker/bin/refresh-mysql-database.php
