35aa5cb23a1ffa36b06abeebe1ed9f1134d73749
Rubens Panfili <rubens.panfili@gmail.com>
2018-12-18 19:27:30 +0100

    fix #696 parsing vhost from amqp dsn

--- pkg/amqp-tools/Tests/ConnectionConfigTest.php
+++ pkg/amqp-tools/Tests/ConnectionConfigTest.php
@@ -534,5 +534,31 @@ class ConnectionConfigTest extends TestCase
                 'ssl_passphrase' => '',
             ],
         ];
+
+        yield [
+            'amqp://guest:guest@localhost:5672/%2f',
+            [
+                'host' => 'localhost',
+                'port' => 5672,
+                'vhost' => '/',
+                'user' => 'guest',
+                'pass' => 'guest',
+                'read_timeout' => 3.,
+                'write_timeout' => 3.,
+                'connection_timeout' => 3.,
+                'persisted' => false,
+                'lazy' => true,
+                'qos_prefetch_size' => 0,
+                'qos_prefetch_count' => 1,
+                'qos_global' => false,
+                'heartbeat' => 0.0,
+                'ssl_on' => false,
+                'ssl_verify' => true,
+                'ssl_cacert' => '',
+                'ssl_cert' => '',
+                'ssl_key' => '',
+                'ssl_passphrase' => '',
+            ],
+        ];
     }
 }
