3a1b758cfefbc2bc706739b22f20faf3d6df71bc
TiMESPLiNTER <github@timesplinter.ch>
2019-09-26 14:55:46 +0200

    Remove unused use

--- pkg/rdkafka/JsonSerializer.php
+++ pkg/rdkafka/JsonSerializer.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Enqueue\RdKafka;
 
-use RdKafka\Message as VendorMessage;
-
 /**
  * @deprecated Use ProduceMessageTransformer and/or ConsumeMessageTransformer instead
  */
