e6b418902ff0bb7de409181d81a384eeecbe26ca
Andrew M-Y <andrejs@gmail.com>
2021-01-19 01:39:14 +0200

    Add file diff

--- bin/git-find-changed-php-files.sh
+++ bin/git-find-changed-php-files.sh
@@ -12,5 +12,4 @@ IFS='
 ALL_CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB "$1");
 PKG_PHP_CHANGED_FILES=$(echo "$ALL_CHANGED_FILES" | grep -E "^pkg\/" | grep -E ".*?\.php$");
 
-echo "$ALL_PHP_CHANGED_FILES";
-#echo "$PKG_PHP_CHANGED_FILES";
+echo "$PKG_PHP_CHANGED_FILES";
