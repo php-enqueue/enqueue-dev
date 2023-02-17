6545ef7907151bdebca77dfe4d57ebc4502cdf74
Andrew M-Y <andrejs@gmail.com>
2023-02-17 19:38:31 +0200

    Add unit tests

--- pkg/snsqs/Tests/Spec/SnsQsContextTest.php
+++ pkg/snsqs/Tests/Spec/SnsQsContextTest.php
@@ -3,13 +3,47 @@
 namespace Enqueue\SnsQs\Tests\Spec;
 
 use Enqueue\Sns\SnsContext;
+use Enqueue\Sns\SnsSubscribe;
 use Enqueue\SnsQs\SnsQsContext;
+use Enqueue\SnsQs\SnsQsQueue;
+use Enqueue\SnsQs\SnsQsTopic;
 use Enqueue\Sqs\SqsConsumer;
 use Enqueue\Sqs\SqsContext;
 use Interop\Queue\Spec\ContextSpec;
 
 class SnsQsContextTest extends ContextSpec
 {
+    public function testSetsSubscriptionAttributes(): void
+    {
+        $topic = new SnsQsTopic('topic1');
+
+        $snsContext = $this->createMock(SnsContext::class);
+        $snsContext->expects($this->once())
+            ->method('setSubscriptionAttributes')
+            ->with($this->equalTo(new SnsSubscribe(
+                $topic,
+                'queueArn1',
+                'sqs',
+                false,
+                ['attr1' => 'value1'],
+            )));
+
+        $sqsContext = $this->createMock(SqsContext::class);
+        $sqsContext->expects($this->any())
+            ->method('createConsumer')
+            ->willReturn($this->createMock(SqsConsumer::class));
+        $sqsContext->expects($this->any())
+            ->method('getQueueArn')
+            ->willReturn('queueArn1');
+
+        $context = new SnsQsContext($snsContext, $sqsContext);
+        $context->setSubscriptionAttributes(
+            $topic,
+            new SnsQsQueue('queue1'),
+            ['attr1' => 'value1'],
+        );
+    }
+
     protected function createContext()
     {
         $sqsContext = $this->createMock(SqsContext::class);
