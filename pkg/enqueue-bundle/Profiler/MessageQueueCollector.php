12a897499e5c45a81677432167f6918c9c1713cf
Nikita Bolotov <nikita.bolotov@betterme.world>
2024-06-07 17:15:42 +0300

    MessageQueueCollector return type

--- pkg/enqueue-bundle/Profiler/MessageQueueCollector.php
+++ pkg/enqueue-bundle/Profiler/MessageQueueCollector.php
@@ -8,7 +8,7 @@ use Throwable;
 
 class MessageQueueCollector extends AbstractMessageQueueCollector
 {
-    public function collect(Request $request, Response $response, Throwable $exception = null)
+    public function collect(Request $request, Response $response, Throwable $exception = null): void
     {
         $this->collectInternal($request, $response);
     }
