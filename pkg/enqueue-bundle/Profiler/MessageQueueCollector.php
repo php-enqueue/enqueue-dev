ee0ab0bc80b38af560aedfc7ff8039760ac493b9
Nikita Bolotov <nikita.bolotov@betterme.world>
2024-06-11 18:39:23 +0300

    fixed unit tests & code style

--- pkg/enqueue-bundle/Profiler/MessageQueueCollector.php
+++ pkg/enqueue-bundle/Profiler/MessageQueueCollector.php
@@ -4,11 +4,10 @@ namespace Enqueue\Bundle\Profiler;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
 class MessageQueueCollector extends AbstractMessageQueueCollector
 {
-    public function collect(Request $request, Response $response, Throwable $exception = null): void
+    public function collect(Request $request, Response $response, ?\Throwable $exception = null): void
     {
         $this->collectInternal($request, $response);
     }
