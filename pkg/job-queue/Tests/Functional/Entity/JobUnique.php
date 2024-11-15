9570303195549a9201c34ddd6ce503b61092aa0b
Viktor Zavitkov <zavitkov@icloud.com>
2024-11-15 20:59:32 +0300

    another tests fixed

--- pkg/job-queue/Tests/Functional/Entity/JobUnique.php
+++ pkg/job-queue/Tests/Functional/Entity/JobUnique.php
@@ -4,15 +4,11 @@ namespace Enqueue\JobQueue\Tests\Functional\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="enqueue_job_queue_unique")
- */
+#[ORM\Entity]
+#[ORM\Table(name: 'enqueue_job_queue_unique')]
 class JobUnique
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(name="name", type="string", nullable=false)
-     */
+    #[ORM\Id]
+    #[ORM\Column(name: 'name', type: 'string', nullable: false)]
     protected $name;
 }
