08f7d883632fdaf71315910706961dba118de16b
James Read <james.read.18@gmail.com>
2025-01-19 18:27:42 +0000

    fix: Fixed language level deprecations

--- pkg/enqueue/Symfony/Client/ConsumeCommand.php
+++ pkg/enqueue/Symfony/Client/ConsumeCommand.php
@@ -94,7 +94,7 @@ class ConsumeCommand extends Command
         try {
             $consumer = $this->getQueueConsumer($client);
         } catch (NotFoundExceptionInterface $e) {
-            throw new \LogicException(sprintf('Client "%s" is not supported.', $client), null, $e);
+            throw new \LogicException(sprintf('Client "%s" is not supported.', $client), 0, $e);
         }
 
         $driver = $this->getDriver($client);
